015a2e5120aa40a865beb45b403ea63b9d027ab3
Ilan Levi <42164441+ilan84levi@users.noreply.github.com>
2019-07-22 19:25:34 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+
+let canvasAnimation = true;
+
 function paintCanvas(height, width, radius, colorHex) {
-    height = width
-    var canvas = document.getElementById("myCanvas").getContext("2d");
+    
+    if(canvasAnimation === true){
+        let canvas = document.getElementById("myCanvas").getContext("2d");
 
-    // Draw Circle:
-    canvas.beginPath();
-    canvas.arc(width, height, radius, 0, 2 * Math.PI); // x-center, y-center, radius, start-angle, end-angle
-    canvas.strokeStyle = colorHex;
-    canvas.stroke();
-
+        // Draw Circle:
+        canvas.beginPath();
+        canvas.arc(width, height, radius, 0, 2 * Math.PI); // x-center, y-center, radius, start-angle, end-angle
+        canvas.strokeStyle = colorHex;
+        canvas.stroke();
+    }
+    else{
+        canvas ="";
+    }
+    
 }
 
 function randomHight() {
@@ -44,8 +52,6 @@ function randomRadius() {
     max = tmp / 2;
     for (var i = 1; i <= 1; i++) {
         radius = parseInt(Math.random() * (max - min + 1) + min);
-        document.getElementById("showRadius").innerText = radius;
-        document.getElementById("showminNumber").innerText = tmp;
         return radius;
 
     }
@@ -63,7 +69,6 @@ function makeRandomColor() {
 
     for (var i = 0; i < 6; i++)
         colorHex += possible.charAt(Math.floor(Math.random() * possible.length + 1));
-    document.getElementById("showHex").innerText = colorHex;
     return colorHex;
 }
 
@@ -75,9 +80,15 @@ function timer() {
     setInterval(parametersCalculate, 800);
 }
 
+
+function playCanvas(){
+    canvasAnimation = true;
+    timer(); 
+}
+
 function stop() {
-    var interval = setInterval(parametersCalculate, 800);
-    clearInterval(interval, 0);
-    clearInterval(timer, 0);
-    clearInterval(paintCanvas, 0);
+    canvasAnimation = false;
+
+   
+
 }
